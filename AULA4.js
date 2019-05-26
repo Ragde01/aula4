e12c8520000d19369508197abf8beb81f79b2bf2
Ragde01 <edgarcastro12@hotmail.com.br>
2019-05-26 19:27:42 +0000

    exercicio

--- AULA4.js
+++ AULA4.js
@@ -1,5 +1,5 @@
 var nome = prompt('Digite seu nome:')
 var idade = prompt('Digite sua idade:')
 
-/*console.log("Meu nome é " + nome + ".")*/
-/*console.log("Minha idade é " + idade + " anos.")*/
+console.log("Meu nome é " + nome + ".")
+console.log("Minha idade é " + idade + " anos.")
