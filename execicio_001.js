baebce108eb37d4fa543bac1e7dd6f57deb0760b
Ragde01 <edgarcastro12@hotmail.com.br>
2019-06-02 17:57:36 -0300

    001

---
+++ execicio_001.js
@@ -0,0 +1,14 @@
+var idade = 20
+    if (idade>0 && idade < 12){
+        console.log("Você é criança !")
+    }
+    else if (idade >=12 && < 18){
+        console.log("Você é adolescente !")
+
+    }
+    else if (idade >=18 && < 40) {
+        console.log("Você é adulto !")
+    }
+    else if (idade >=40){
+        console.log("Você é velho !")
+    }
